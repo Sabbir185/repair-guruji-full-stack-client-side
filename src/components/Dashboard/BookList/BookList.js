4a7b4e3d528ad106dc4bcf8852bda09298c1ec6d
Sabbir Ahmmed <sabbir.cse@yahoo.com>
2021-04-18 13:23:23 +0600

    booking list done

--- src/components/Dashboard/BookList/BookList.js
+++ src/components/Dashboard/BookList/BookList.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { UserContext } from '../../../App';
 import HeaderNavbar from '../../Shared/HeaderNavbar/HeaderNavbar';
 import Sidebar from '../Sidebar/Sidebar';
+import './BookList.css'
 
 const BookList = () => {
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+    const [bookList, setBookList] = useState([]);
+
+    useEffect(() => {
+        fetch(`http://localhost:5055/getBooking?email=${loggedInUser.email}`)
+            .then(res => res.json())
+            .then(data => setBookList(data))
+    }, []);
+
+
     return (
         <section className="container">
             <HeaderNavbar></HeaderNavbar>
@@ -13,7 +25,30 @@ const BookList = () => {
                 </div>
                 <div className="col  ml-5">
                     <h4 className="pb-3">Book You Service</h4>
-                    <h1>BookList</h1>
+
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Service Type</th>
+                                <th>Client Email</th>
+                                <th>Payment Id</th>
+                                <th>Status</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                bookList.map(book =>
+                                    <tr>
+                                        <td>{book.title}</td>
+                                        <td>{book.email}</td>
+                                        <td>{book.paymentId}</td>
+                                        <td>Done</td>
+                                    </tr>
+                                )
+                            }
+                        </tbody>
+                    </table>
+
                 </div>
             </div>
         </section>
